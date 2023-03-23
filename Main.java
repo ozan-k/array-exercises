7528cd8f94b5c1084522163e5939a03b4c8adb24
ozan-k <ozan.kah@gmail.com>
2023-03-23 20:18:39 +0100

    exercise2

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        ResizableArray a = new ResizableArray();
+
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.add(40);
+
+        System.out.println(a);
+    }
+}
